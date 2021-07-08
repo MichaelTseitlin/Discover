5fe4e90282e989fffcd5e4ef8eda634aa937e893
Michael Tseitlin <tseytlin21@gmail.com>
2021-07-08 11:51:16 +0300

    Initial Commit

---
+++ Discover/DiscoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverApp.swift
+//  Discover
+//
+//  Created by Michael Tseitlin on 08.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct DiscoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
